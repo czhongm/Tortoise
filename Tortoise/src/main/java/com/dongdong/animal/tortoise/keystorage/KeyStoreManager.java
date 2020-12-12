48087294e92cf48425b7e5b86a303b8decc4ecfd
czm <czhongm@gmail.com>
2020-12-12 13:36:57 +0800

    feat: migrate to AndroidX

--- Tortoise/src/main/java/com/dongdong/animal/tortoise/keystorage/KeyStoreManager.java
+++ Tortoise/src/main/java/com/dongdong/animal/tortoise/keystorage/KeyStoreManager.java
@@ -6,7 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.security.KeyPairGeneratorSpec;
 import android.security.keystore.KeyProperties;
-import android.support.annotation.RequiresApi;
+import androidx.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.Base64;
 
--- app/src/main/java/com/dongdong/animal/app/MainActivity.java
+++ app/src/main/java/com/dongdong/animal/app/MainActivity.java
@@ -1,13 +1,12 @@
 package com.dongdong.animal.app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.dongdong.animal.app.ui.aesuse.AesUseActivity;
 import com.dongdong.animal.app.ui.spsafeuse.SafeSpActivity;
-import com.dongdong.animal.tortoise.confusion.ConfusionUtil;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
